c22737a50df7ca0f631163745a6433bed8817ae5
Joandysson Gama <joandysson.gama@gmail.com>
2020-09-06 12:56:43 -0300

    ajustes em gulpfile and licende

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-var gulp = require("gulp"),
-  sass = require("gulp-sass"),
-  sourcemaps = require("gulp-sourcemaps"),
-  autoprefixer = require("gulp-autoprefixer"),
-  rename = require("gulp-rename"),
-  concat = require("gulp-concat"),
-  cleanCSS = require("gulp-clean-css"),
-  uglify = require("gulp-uglify");
+var gulp = require("gulp")
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
+const concat = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
 
 function swallowError(error) {
   // If you want details of the error in the console
@@ -13,7 +13,7 @@ function swallowError(error) {
   this.emit("end");
 }
 
-var sassOptions = {
+const sassOptions = {
   errLogToConsole: true,
   outputStyle: "compressed",
 };
@@ -22,17 +22,16 @@ var sassOptions = {
 gulp.task("sass", function () {
   return gulp
     .src([
-      "assets/css/sass/bootstrap/scss/bootstrap.scss",
-      "assets/css/sass/site.scss",
+      "public/assets/css/sass/bootstrap/scss/*.scss",
     ])
     .pipe(sass(sassOptions).on("error", sass.logError))
-    .pipe(gulp.dest("assets/css/"));
+    .pipe(gulp.dest("public/assets/css/"));
 });
 
 // CSS
 gulp.task("css", function () {
   return gulp
-    .src(["assets/css/bootstrap.css", "assets/css/site.css"])
+    .src(["public/assets/css/*.css"])
     .pipe(concat("all.css"))
     .pipe(
       autoprefixer({
@@ -45,33 +44,29 @@ gulp.task("css", function () {
     .pipe(
       sourcemaps.write("/", {
         includeContent: false,
-        sourceRoot: "../../assets/css/sass",
+        sourceRoot: "../../public/assets/css/sass",
       })
     )
-    .pipe(gulp.dest("assets/css/"));
+    .pipe(gulp.dest("public/assets/css/"));
 });
 
 gulp.task("scripts", function () {
   return gulp
     .src([
-      // The order that you list the files in this array IS IMPORTANT!!
-      "assets/js/jquery-3.2.1.min.js",
-      //'assets/js/bootstrap.min.js',
-      //'assets/js/popper.js',
-      "assets/js/main.js",
+      "public/assets/js/*.js",
     ])
     .pipe(concat("all.js"))
     .pipe(rename({ suffix: ".min" }))
     .pipe(uglify())
     .on("error", swallowError)
-    .pipe(gulp.dest("assets/js/"));
+    .pipe(gulp.dest("public/assets/js/"));
 });
 
 //Watch Task
 gulp.task("watch", function () {
-  gulp.watch("assets/js/main.js", gulp.series("scripts"));
-  gulp.watch("assets/css/sass/site.scss", gulp.series("sass"));
-  gulp.watch("assets/css/site.css", gulp.series("css"));
+  gulp.watch("public/assets/js/main.js", gulp.series("scripts"));
+  gulp.watch("public/assets/css/sass/site.scss", gulp.series("sass"));
+  gulp.watch("public/assets/css/site.css", gulp.series("css"));
 });
 
 //Default task
