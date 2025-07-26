3700a2126987cd88efdb2afa75d77d6aac6edf4f
Joandysson Gama <joandysson.gama@gmail.com>
2025-07-26 11:22:39 -0300

    docs: add phpdoc to request and response facades

--- app/Config/Response/ResponseFacade.php
+++ app/Config/Response/ResponseFacade.php
@@ -5,6 +5,17 @@ namespace App\Config\Response;
 
 use BadMethodCallException;
 
+/**
+ * Static proxy to {@see Response} allowing fluent response building.
+ *
+ * @method static self setStatus(int|HttpStatus $status)
+ * @method static self addHeader(string $name, string $value)
+ * @method static void json(array $data, int|HttpStatus $status = HttpStatus::OK)
+ * @method static void view(string $view, array $data = [], int|HttpStatus $status = HttpStatus::OK)
+ * @method static void send(string $content, int|HttpStatus $status = HttpStatus::OK)
+ * @method static int getStatus()
+ * @method static array getHeaders()
+ */
 class ResponseFacade
 {
     public static function __callStatic(string $name, array $arguments): mixed
