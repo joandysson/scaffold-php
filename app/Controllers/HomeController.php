c00a1cc3e76cbcc7aac16980d3fa25a1281a8909
joandysson <joandysson.gama@gmail.com>
2025-07-26 18:22:38 -0300

    fix

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -9,8 +9,6 @@ class HomeController
 {
     public function home(Request $request): void
     {
-        xdebug_break();
-        xdebug_info(); exit;
             echo "Welcome to the Home Page!\n";
 
             // (new \App\Config\Response\Response())->view(
